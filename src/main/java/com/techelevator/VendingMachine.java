e8070ff499b42e90d07d10553e1b8c5afaa03b01
Quinn di Girolamo <quinndigi@gmail.com>
2022-02-12 19:22:04 -0500

    Evening Push before meeting with Tom

--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -1,14 +1,11 @@
 package com.techelevator;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.TreeMap;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class VendingMachine {
     List<String> list = new ArrayList<String>();
@@ -58,18 +55,66 @@ public class VendingMachine {
         return this.list;
     }
 
-    public void logFile() throws IOException {
+    public void logTransaction() throws IOException {
         File purchaseRecord = new File("src/test/resources/Log.txt");
-        List<String> list = getList();
-        try(FileWriter logWriter = new FileWriter(purchaseRecord, true)){
-            for(String string : list) {
-                logWriter.write(string);
-                logWriter.write("\n");
-            }
+
+        try(PrintWriter logWriter = new PrintWriter(purchaseRecord){
+
+            LocalDateTime dateTime = LocalDateTime.now(); //Gets the current date and time
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss a");
+            //dd = day of month, MM = month of year, yyyy = year of era, HH = hour of day, mm = minute of hour, ss = second of minute, a = am or pm of day
+
+
+            //TODO: Need to find a way to show if we use the method feedMoney to return "FEED MONEY"
+            //TODO: Need to find a way to get the product name and the location!
+            //TODO: Need to find a way to get the starting balance and ending balance
+            //TODO: Need to find a war to show if we use the method getChange to return "GIVE CHANGE"
+
+
+            String actionProductFormat = String.format("%s, %d ", inventory.getInventory(productName), location);
+            // -> String logPrint = dateTime + " " + {Action} + " " + {Starting Balance} + " " + {Ending Balance}
+            // -> println(logPrint);
+            // (product name, location)
         }
+        }
+}
+//                 Example Given:
+//                    >01/01/2016 12:00:15 PM FEED MONEY: $5.00 $10.00
+//                    >01/01/2016 12:00:20 PM Crunchie B4 $10.00 $8.50
+//                    >01/01/2016 12:01:25 PM Cowtales B2 $8.50 $7.50
+//                    >01/01/2016 12:01:35 PM GIVE CHANGE: $7.50 $0.00
+
+        //TODO (Date -> Time -> Action -> Balance At Start -> Balance at End)
+            //Example:  01/01/2016 12:00:00 PM FEED MONEY: $5.00 $5.00
+
+            //Action is feedMoney {customerBalance}, selectProduct {product}, or
+
+//   List<String> list = getList();
+//        public void logTransactions() {
+
+
+//        String destinationFile = "src/test/resources/Log.txt";
+//        File purchaseRecord = new File(destinationFile);
+//        boolean append = destinationFile.exists() ? true : false;
+
+
+
+//        List<String> list = getList();
+//        try(FileWriter logWriter = new FileWriter(purchaseRecord, true)){
+//            for(String string : list) {
+//                logWriter.write(string);
+//                logWriter.write("\n");
+//            }
+//        }
+
+//        File destinationFile = new File(destinationPath);
+//
+//        boolean append = destinationFile.exists() ? true : false;
+//
+//        try(PrintWriter output = new PrintWriter(new FileOutputStream(destinationFile, append))) {
 
 //        public List<String> log(
-    }
+
 
 //    SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
 //    Date date = new Date(System.currentTimeMillis());
--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -8,8 +8,15 @@ public class VendingMachineCLI {
 
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-	private static final String PURCHASE_MENU_OPTIONS = "Menu Options";
+	private static final String MAIN_MENU_OPTION_EXIT = "Exit To Main Menu";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE};
+
+	private static final String PURCHASE_MENU_FEED_MONEY = "Feed Money";
+	private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
+	private static final String PURCHASE_MENU_OPTION_DEPOSIT_MONEY = "Deposit Money";
+	private static final String PURCHASE_MENU_OPTION_END_TRANSACTION = "End Transaction";
+	private static final String[] PURCHASE_MENU_OPTIONS = {PURCHASE_MENU_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT,
+			 PURCHASE_MENU_OPTION_DEPOSIT_MONEY, PURCHASE_MENU_OPTION_END_TRANSACTION };
 
 	private Menu menu;
 
@@ -18,23 +25,36 @@ public class VendingMachineCLI {
 	}
 
 	public void run() {
-//		VendingMachine vm = new VendingMachine(new File("vendingmachine.csv"));
-//
-//		String[] activeMenu = MAIN_MENU_OPTIONS;
-//
-//		boolean run = true;
-//
-//		while (true) {
-//			String choice = (String) menu.getChoiceFromOptions(activeMenu);
-//
-//			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-//				vm.getInventory();
-//				// display vending machine items
-//			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-//				activeMenu = PURCHASE_MENU_OPTIONS;
-//				// do purchase
-//			}
-//		}
+		VendingMachine vm = new VendingMachine(new File("vendingmachine.csv"));
+
+		String[] activeMenu = MAIN_MENU_OPTIONS;
+
+		boolean run = true;
+
+		while (run) {
+			String choice = (String) menu.getChoiceFromOptions(activeMenu);
+
+			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
+				vm.getInventory();
+				// display vending machine items
+			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+				activeMenu = PURCHASE_MENU_OPTIONS;
+				// do purchase
+			} else if (userChoice.equals(MAIN_MENU_OPTION_EXIT)) {
+				vm.exitDialog();
+				run = false;
+			} else if (userChoice.equals(PURCHASE_MENU_OPTION_DEPOSIT_MONEY)) {
+				String deposit = (String) menu.getChoiceFromOptions(vm.currentInventory.keySet().toArray(), true);
+				vm.makeDeposit(deposit);
+			} else if (userChoice.equals(PURCHASE_MENU_OPTION_SELECT_PRODUCT)) {
+				vm.listItems();
+				String product = (String) menu.getChoiceFromOptions(vm.currentInventory.keySet().toArray(), true);
+				vm.dispense(product);
+			} else if (userChoice.equals(PURCHASE_MENU_OPTION_END_TRANSACTION)) {
+				vm.closeBank();
+				activeMenu = MAIN_MENU_OPTIONS;
+			}
+		}
 	}
 
 	public static void main(String[] args) {
